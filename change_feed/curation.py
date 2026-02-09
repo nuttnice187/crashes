9c5737dbefeec08cebc8c72883edf24b0452fed6
Phillip Andrew Raywood <thephillipraywood@gmail.com>
2026-02-09 15:00:09 -0500

    Update and rename silver.py to curation.py

---
+++ change_feed/curation.py
@@ -0,0 +1,8 @@
+def main():
+    """
+    Read source data ingested to cloud storage:
+    1.  Transform Data Types
+    2.  Add calculated columns, ranked window functions, etc
+    3.  Write to silver delta table, by "run_id"
+    """
+    pass
