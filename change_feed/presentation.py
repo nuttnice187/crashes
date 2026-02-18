7150a123ece75eb0adcd13a47cb2fcf95a415a17
nuttnice187 <thephillipraywood@gmail.com>
2026-02-18 19:23:52 +0000

    Update presentation.py

--- change_feed/presentation.py
+++ change_feed/presentation.py
@@ -1,7 +1,13 @@
-def main():
+from argparse import Namespace
+from logging import Logger
+from pyspark.sql import SparkSession
+
+
+def main(spark: SparkSession, logger: Logger, args: Namespace) -> None:
     """
     Read from curated delta table and apply any required
     1. Filter to specific level of detail
     2. Aggregation, summarization
-    """    
+    3. Write to bronze delta table
+    """
     pass
